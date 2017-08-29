21ca4175bf902ae3da6cff3144d2d2965cc80944
jdfellman <fellmanjames@gmail.com>
2017-08-29 15:34:21 -0500

    Inital commit for Spring boot project

---
+++ src/main/java/com/devopshelper/DevopshelperApplication.java
@@ -0,0 +1,12 @@
+package com.devopshelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopshelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopshelperApplication.class, args);
+	}
+}
---
+++ src/test/java/com/devopshelper/DevopshelperApplicationTests.java
@@ -0,0 +1,16 @@
+package com.devopshelper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DevopshelperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
